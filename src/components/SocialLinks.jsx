eeba571a86b2b0f991010804980b466e0fed2205
EchoNG2T <ngtsztat.echo@gmail.com>
2024-06-19 17:43:45 +0800

    pdf file name change

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -44,7 +44,7 @@ const SocialLinks = () => {
           <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/NGTszTat(Echo)_Resume.pdf",
+      href: "/ngtsztat_resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
